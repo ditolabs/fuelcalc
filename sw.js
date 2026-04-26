2f1ff4d0f607dd1aec10603f618e7f9076a4c733
ditolabs <halodito@gmail.com>
2026-04-26 09:48:35 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'bbm-v1';
+const ASSETS = [
+  '/kalkulator-bbm/',
+  '/kalkulator-bbm/index.html',
+  '/kalkulator-bbm/manifest.json',
+  '/kalkulator-bbm/icon.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
